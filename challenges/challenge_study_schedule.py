85ad2d6a1899de4136609e546103ae3d2d04ec75
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-08-18 14:49:45 -0300

    REQ1 - Finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (target_time is None):
+        return None
+    students = 0
+    for student in permanence_period:
+        if type(student[0]) != int or type(student[1]) != int:
+            return None
+        time = list(range(student[0], student[1] + 1))
+        if target_time in time:
+            students += 1
+    return students
