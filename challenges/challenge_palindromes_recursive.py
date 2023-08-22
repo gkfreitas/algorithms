55ba325b77fd9fe845515633dae161e269d32917
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-08-22 08:01:38 -0300

    REQ02 - Finalizado

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    print(type(word), len(word))
+    if type(word) == str and len(word) == 0:
+        return False
+    if len(word) == 1 or len(word) == 0:
+        return True
+    if word[low_index] == word[high_index]:
+        listWord = list(word)
+        del listWord[high_index]
+        del listWord[low_index]
+        newHighIndex = len(listWord) - 1
+        return is_palindrome_recursive(listWord, low_index, newHighIndex)
+    return False
