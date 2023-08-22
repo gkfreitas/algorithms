1bc97f5dd6620ba02b531c04cc1402dc4309c66e
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-08-22 08:54:52 -0300

    REQ04 - Finalizado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,40 @@
+def merge_sort(word, start=0, end=None):
+    if end is None:
+        end = len(word)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(word, start, mid)
+        merge_sort(word, mid, end)
+        merge(word, start, mid, end)
+
+
+def merge(word, start, mid, end):
+    left = word[start:mid]
+    right = word[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            word[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            word[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            word[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            word[general_index] = right[right_index]
+            right_index = right_index + 1
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(first_string) == 0 and len(second_string) == 0:
+        return (first_string, second_string, False)
+    first_string = list(first_string.lower())
+    second_string = list(second_string.lower())
+    merge_sort(first_string, 0, len(first_string))
+    merge_sort(second_string, 0, len(second_string))
+    verify = first_string == second_string
+    return ("".join(first_string), "".join(second_string), verify)
